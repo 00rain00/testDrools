39fb8c5ec4b892111a08318957daa4c7d84be3d1
irvine <irvine@ouyang>
2019-10-29 03:07:20 +0900

    add limit for comments
    todo change background   imrpve conts

--- src/gamescene/Play.java
+++ src/gamescene/Play.java
@@ -23,13 +23,19 @@ import manager.SoundManager;
 import py4j.Py4JException;
 import setting.FlagSetting;
 import setting.GameSetting;
+import struct.CharacterData;
 import struct.FrameData;
 import struct.GameData;
 import struct.ScreenData;
 import util.DebugActionData;
 import util.LogWriter;
 import util.ResourceDrawer;
-
+import comment.Message;
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+import comment.Fightbot;
+import com.cavariux.twitchirc.Chat.*;
 /**
  * 対戦中のシーンを扱うクラス．
  */
@@ -93,6 +99,14 @@ public class Play extends GameScene {
 	/**
 	 * クラスコンストラクタ．
 	 */
+	
+	 static KieServices ks;
+	 static  KieContainer kContainer;
+	 static KieSession kSession;
+	 static Message msg;
+	 static int commentLimit;
+	 public Fightbot fbot;
+	public  Channel channel;
 	public Play() {
 		// 以下4行の処理はgamesceneパッケージ内クラスのコンストラクタには必ず含める
 		this.gameSceneName = GameSceneName.PLAY;
@@ -121,7 +135,7 @@ public class Play extends GameScene {
 		this.roundResults = new ArrayList<RoundResult>();
 
 		this.timeInfo = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy.MM.dd-HH.mm.ss", Locale.ENGLISH));
-
+		
 		if (!FlagSetting.trainingModeFlag) {
 			openReplayFile();
 		}
@@ -148,7 +162,19 @@ public class Play extends GameScene {
 		if (FlagSetting.enableWindow && !FlagSetting.muteFlag) {
 			SoundManager.getInstance().play(SoundManager.getInstance().getBackGroundMusic());
 		}
+		this.fbot = new Fightbot();
+		String cName = "#hunteer_999";
+		
+		try {
+			System.out.println("initial bot connections :"+cName);
+			fbot.connect();
+			 channel = fbot.joinChannel(cName);
+			
+		} catch(Exception e) {
+			Logger.getAnonymousLogger().log(Level.SEVERE, "Cannot connect to twitch");
+		}
 	}
+	
 
 	@Override
 	public void update() {
@@ -202,8 +228,12 @@ public class Play extends GameScene {
 		this.roundStartFlag = false;
 		this.elapsedBreakTime = 0;
 		this.keyData = new KeyData();
-		
+		commentLimit = 50;
 		InputManager.getInstance().clear();
+		 ks = KieServices.Factory.get();
+	     kContainer = ks.getKieClasspathContainer();
+	      kSession = kContainer.newKieSession("ksession-rules");
+	      msg = new Message();
 	}
 
 	/**
@@ -238,7 +268,51 @@ public class Play extends GameScene {
 		this.frameData = this.fighting.createFrameData(this.nowFrame, this.currentRound);
 		//log frameData
 		
-		// リプレイログ吐き出し
+			   try {
+			   FrameData currentFrameData = this.frameData;
+			  
+			   if(currentFrameData.currentFrameNumber>1&&currentFrameData.currentFrameNumber%10==0) {
+				//   i=i+1;
+				   System.out.println(currentFrameData.currentFrameNumber+"::::::::");
+				   CharacterData p1 =  currentFrameData.getCharacter(true);
+				   CharacterData p2 =  currentFrameData.getCharacter(false);
+//				  
+				   kSession.insert(p1);
+				   kSession.insert(p2);
+				   kSession.insert(msg);
+				   
+				  
+				  
+				  
+				   kSession.fireAllRules();
+			  		
+				  
+				   //check limit
+				   if(commentLimit>0) {
+					   System.out.println("comment limit :"+commentLimit);
+					   ArrayList<String> comments = msg.getComments();
+				  		for(String com:comments) {
+				  			System.out.println(com);
+				  			commentLimit = commentLimit-1;
+				  			fbot.sendMessage(com, channel);
+				  		}
+				  		msg.emptyComments();
+				   }else {
+					   System.out.println("over limit");
+				   }
+				   //nearly 30s  50 per 30s 
+				   if(currentFrameData.currentFrameNumber>=1700) {
+					   commentLimit = 50;
+				   }
+				   
+				   
+				   
+				   
+			   }}catch(Exception ex) {
+				   System.out.println(ex);
+			   }
+		   
+		// リレイログ吐き出し
 		if (!FlagSetting.trainingModeFlag) {
 			LogWriter.getInstance().outputLog(this.dos, this.keyData, this.fighting.getCharacters());
 		}
