ad7242dc066a613519591bfa83f939bf6c5b1755
cy <caiyulinhunteer@gmail.com>
2019-10-28 23:38:03 +0900

    add twitch

---
+++ src/comment/Fightbot.java
@@ -0,0 +1,10 @@
+package comment;
+import com.cavariux.twitchirc.Core.TwitchBot;
+public class Fightbot extends TwitchBot {
+	public Fightbot ()
+	{
+		this.setUsername("hunteer_999"); //this.setUsername("CavsBot"); 
+		this.setOauth_Key("oauth:xwr9bmj9ys7i4ciy5ins29uqh3iwg0"); //this.setOauth_Key("oauth:9kvd020oj3wgcrsyafoaofrt3uv7bi");
+	}
+	
+}
